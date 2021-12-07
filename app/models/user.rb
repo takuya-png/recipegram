a7e26916b19553c824f32562df0037499f474c27
takuya nagasawa <tsukasayanagawa474@gmail.com>
2021-12-07 23:17:44 +0900

    アソシエーション追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   attachment :profile_image
+  has_many :recipes, dependent: :destroy
 end
