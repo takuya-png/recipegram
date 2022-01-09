42c2507d415e08198b6b415386b9d17e2dad54bb
takuya nagasawa <tsukasayanagawa474@gmail.com>
2022-01-09 23:36:00 +0900

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   attachment :profile_image
   has_many :recipes, dependent: :destroy
-
   validates :username, presence: true
 end
