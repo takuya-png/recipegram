5f1bf6c75683f5484bf06d5a025455b3d1a252c5
takuya nagasawa <tsukasayanagawa474@gmail.com>
2022-01-15 16:20:46 +0900

    いいね機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#index"
   resources :users
-  resources :recipes
+  resources :recipes do
+    resource :favorites, only: [:create, :destroy]
+  end
 end
