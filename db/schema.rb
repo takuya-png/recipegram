d0301a8cd71f159c233a9988851d1fe94b6cf421
takuya nagasawa <tsukasayanagawa474@gmail.com>
2022-01-15 15:57:20 +0900

    favoriteモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_07_141110) do
+ActiveRecord::Schema.define(version: 2022_01_15_065524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "recipe_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.integer "user_id"
