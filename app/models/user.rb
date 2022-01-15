8b6c8df4f33f93edcdeb0c40aa4ec4356646486f
takuya nagasawa <tsukasayanagawa474@gmail.com>
2022-01-15 16:12:44 +0900

    userモデルにメソッド追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
   has_many :recipes, dependent: :destroy
   has_many :favorites, dependent: :destroy
   validates :username, presence: true
+
+  def already_favorited?(recipe)
+    self.favorites.exists?(recipe_id: recipe.id)
+  end
 end
